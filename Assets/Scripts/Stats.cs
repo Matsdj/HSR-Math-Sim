8a3b652b2ed6e0c68d7b6bfe7f8b51de9d67389f
Ivar de Jong <Ivardj@xs4all.nl>
2023-12-26 00:28:13 +0100

    basestats additions

--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -7,23 +7,56 @@ using UnityEngine;
 [Serializable]
 public class BaseStats
 {
+    //At lvl 1
     public float HP;
     public float ATK;
     public float DEF;
     public float SPD;
+
+    [HideInInspector] public float finalHP;
+    [HideInInspector] public float finalATK;
+    [HideInInspector] public float finalDEF;
+    [HideInInspector] public float finalSPD;
+
+    [HideInInspector] public float percentHP = 0;
+    [HideInInspector] public float flatHP = 0;
+    [HideInInspector] public float percentATK = 0;
+    [HideInInspector] public float flatATK = 0;
+    [HideInInspector] public float percentDEF = 0;
+    [HideInInspector] public float flatDEF = 0;
+    [HideInInspector] public float percentSPD = 0;
+    [HideInInspector] public float flatSPD = 0;
+    public void FinalStats(int lvl, int ascension, string LightCone, string[] EquipmentStats)
+    {
+        //At current lvl
+        float tempHP = HP + HP * 0.05f * (lvl - 1) + HP * 0.4f * (ascension);
+        float tempATK = ATK + ATK * 0.05f * (lvl - 1) + ATK * 0.4f * (ascension);
+        float tempDEF = DEF + DEF * 0.05f * (lvl - 1) + DEF * 0.4f * (ascension);
+        float tempSPD = SPD;
+
+
+        //TODO Lightcone
+
+        //TODO Equipment
+
+        finalHP = tempHP * (1 + percentHP);
+        finalATK = tempATK * (1 + percentATK);
+        finalDEF = tempDEF * (1 + percentDEF);
+        finalSPD = tempSPD * (1 + percentSPD);
+    }
 }
 
 /// <summary> CRIT rate+dmg, Break, Healboost, Energy Max+rate, Effect Hit+RES </summary>
 [Serializable]
 public class AdvancedStats
 {
-    public float CRIT_Rate = 5,
+    [HideInInspector] public float CRIT_Rate = 5,
         CRIT_DMG = 50,
         BreakEffect,
         OutgoingHealingBoost,
-        MaxEnergy,
         EnergyRegenerationRate,
         EffectHitRate,
-        EffectRES,
+        EffectRES;
+    public float MaxEnergy,
         Aggro;
 }
