107b9f8621778254f40f1ed87b54c9cf1de848a0
Matsdj <mats2002.com@gmail.com>
2023-12-25 20:39:52 +0100

    Pictures for characters

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Character", menuName = "ScriptableObjects/Character", order = 1)]
 public class Character : ScriptableObject
 {
+    public Sprite sprite;
     public int LVL = 80;
     public BaseStats baseStats;
     public AdvancedStats advancedStats;
