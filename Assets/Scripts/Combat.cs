85c0940a066d619a78de1c0b080a0da6da1f0a8e
Matsdj <mats2002.com@gmail.com>
2024-02-20 18:41:17 +0100

    Base action order logic done

--- Assets/Scripts/Combat.cs
+++ Assets/Scripts/Combat.cs
@@ -7,20 +7,68 @@ public class Combat : MonoBehaviour
 {
     public Character[] Allies;
     public Character[] Enemies;
-    public Queue<Character> ActionOrder;
+    public List<ActionCharacter> ActionOrder;
     public List<string> ActionHistory;
     public HorizontalLayoutGroup AllyUI;
     public HorizontalLayoutGroup EnemyUI;
     public VerticalLayoutGroup ActionOrderUI;
     public VerticalLayoutGroup ActionHistoryUI;
+    private float _latestActionValue = 0;
 
-    void Start()
+    public void Awake()
     {
-        
+        ActionOrder = new List<ActionCharacter>();
+        foreach (Character character in Allies)
+        {
+            AddCharacter(character);
+        }
+        foreach (Character character in Enemies)
+        {
+            AddCharacter(character);
+        }
+        SortActionOrder();
     }
 
-    void Update()
+    public void AddCharacter(Character character)
     {
-        
+        ActionCharacter action = new ActionCharacter(character, _latestActionValue);
+        ActionOrder.Add(action);
+    }
+
+    public void DoTurn(ActionCharacter action)
+    {
+        action.DoTurn();
+        SortActionOrder();
+    }
+
+    public void SortActionOrder()
+    {
+        ActionOrder.Sort((a, b) => a.ActionValue.CompareTo(b.ActionValue));
+
+        string debug = "ActionOrder: ";
+        foreach (ActionCharacter action in ActionOrder)
+        {
+            debug += $"(Name:{action.Character.name}, ActionValue:{action.ActionValue}), ";
+        }
+        Debug.Log(debug);
+    }
+
+    public class ActionCharacter
+    {
+        public Character Character;
+        public float ActionValue;
+
+        public ActionCharacter(Character character, float actionValue)
+        {
+            Character = character;
+            ActionValue = actionValue;
+            DoTurn();
+        }
+
+        public void DoTurn()
+        {
+            Character.baseStats.CalculateFinalSPD();
+            ActionValue += 10000 / Character.baseStats.finalSPD;
+        }
     }
 }
